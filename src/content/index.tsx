e0c1990e8a57d19ff08cdcc894bd220632c29b5b
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2024-02-10 16:34:55 +0900

    ui: display dialog neer the cursor

--- src/content/index.tsx
+++ src/content/index.tsx
@@ -1,11 +1,18 @@
 import { createRoot } from 'react-dom/client';
 import '@mantine/core/styles.css';
-import { createTheme, MantineProvider } from '@mantine/core';
+import { createTheme, MantineProvider, Box, List } from '@mantine/core';
 
 const theme = createTheme({
 });
 
 console.log('content script');
+
+let mouseX  = 0;
+let mouseY  = 0;
+document.addEventListener('contextmenu', (event) => {
+  mouseX = event.clientX;
+  mouseY = event.clientY;
+}, true);
 
 chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
 // TODO messageに型をつける
@@ -15,7 +22,6 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
   console.log(sendResponse);
   sendResponse({ farewell: 'goodbye' });
 
-// TODO ポップアップの表示位置をマウスカーソルの近くにする
 // TODO ポップアップを消せるようにする
 // TODO ポップアップをタブで切り替えてレスポンスのXMLを表示できるようにする
 
@@ -26,8 +32,8 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
         style={{
           position: 'absolute',
           width: '100%',
-          left: '0px',
-          top: '0px',
+          left: `${mouseX}px`,
+          top: `${mouseY}px`,
           zIndex: 2147483550,
         }}
       >
@@ -40,13 +46,26 @@ chrome.runtime.onMessage.addListener(function (message, sender, sendResponse) {
             zIndex: 2147483550,
           }}
         >
+          <Box
+            style={(theme) => ({
+              backgroundColor: 'white',
+              textAlign: 'left',
+              padding: theme.spacing.md,
+              borderRadius: theme.radius.md,
+              maxWidth: 500,
+              boxShadow: '0 0 10px rgba(0, 0, 0, .3)',
+              zIndex: 2147483550
+            })}
+            component='div'
+            >
             <div>
                 <h1>書誌情報一覧（content/index.tsx）</h1>
                 <h2>検索文字列: TODO</h2>
-                <ul>
-                {message.data.bibliographies.map((item, index) => (<li key={index}>{item.author ?? "N/A"}, 『{item.title ?? "N/A"}』, {item.publisher ?? "N/A"}, {item.publicationYear ?? "N/A"}</li>))}
-                </ul>
+                <List>
+                {message.data.bibliographies.map((item, index) => (<List.Item key={index}>{item.author ?? "N/A"}, 『{item.title ?? "N/A"}』, {item.publisher ?? "N/A"}, {item.publicationYear ?? "N/A"}</List.Item>))}
+                </List>
             </div>
+            </Box>
         </div>
       </div>
       </MantineProvider>
