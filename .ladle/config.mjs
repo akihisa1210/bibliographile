e2e8d544cf741c5450e41874ce94b211d9baf2c6
akihisa1210 <19635263+akihisa1210@users.noreply.github.com>
2024-02-12 10:37:08 +0900

    chore: rename config file

--- .ladle/config.mjs
+++ .ladle/config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('@ladle/react').UserConfig} */
 export default {
   stories: "src/**/*.stories.{js,jsx,ts,tsx,mdx}",
-  viteConfig: process.cwd() + "/ladle-vite.config.ts",
+  viteConfig: process.cwd() + "/vite-ladle.config.ts",
 };
